c2f8a155b8d77593e4bb696593a4414b1413672d
BMS22-23 <bresen@alu.edu.gva.es>
2023-03-16 20:05:06 +0100

    Hecho métodos de prueba para los casos en que me debe dar false y además he añadido el dossier a la misma carpeta donde se encuentra este proyecto

---
+++ Loto/UnitTestConstructorParametro/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestConstructorParametro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestConstructorParametro")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20df2d52-51db-4cb5-896c-d7321fee43ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Loto/UnitTestConstructorParametro/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LotoClassNS;
+
+namespace UnitTestConstructorParametro
+{
+    [TestClass]
+    public class UnitTest1
+    {        
+        [DataTestMethod]
+        [DataRow(0, false)]
+        public void ConstructorParametroMisNumMenorA1(int[] numeros, bool esValida)
+        {
+            BMS2223 bMS2223 = new BMS2223(numeros);
+
+            Assert.AreEqual(esValida, false, "Error");
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 6, 7, false)]
+        [DataRow(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, false)]
+        public void ConstructorParametroMisNumMayor6(int[] numeros, bool esValida)
+        {
+            BMS2223 bMS2223 = new BMS2223(numeros);
+
+            Assert.AreEqual(esValida, false, "Error");
+        }
+    }
+}
